a7e9285094ff46351cfb9b464275cdcc132c9db0
flwkemd <flwkemd@naver.com>
2017-12-07 03:07:19 +0100

    Hibernate Advanced Mappings - @OneToOne

---
+++ src/com/ex/hibernate/oneToone/CreateDemo.java
@@ -0,0 +1,54 @@
+package com.ex.hibernate.oneToone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ex.hibernate.oneToone.entity.Instructor;
+import com.ex.hibernate.oneToone.entity.InstructorDetail;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Instructor.class)
+								 .addAnnotatedClass(InstructorDetail.class)
+								 .buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//create the objects
+			//Instructor tempInstructor = new Instructor("Kim", "Ho", "flwkemd@naver.com");
+			//InstructorDetail tempInstructorDetail = new InstructorDetail("www.abc.com", "coding");
+
+			Instructor tempInstructor = new Instructor("Lee", "Jun", "aldhfl@naver.com");
+			InstructorDetail tempInstructorDetail = new InstructorDetail("www.cal.com", "Good");
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			// Note : this will also save the details object
+			// because of CascadeType.ALL
+			System.out.println("Saving instructor: "+tempInstructor);
+			session.save(tempInstructor);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}finally {
+			factory.close();
+		}
+		
+	}
+
+}
--- src/com/ex/jdbc/TestJdbc.java
+++ src/com/ex/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 public class TestJdbc {
 	public static void main(String[] args) {
 		
-	String jdbcUrl = "jdbc:mysql://localhost:3307/hb_student_tracker?userSSL=false";
+	String jdbcUrl = "jdbc:mysql://localhost:3307/hb-01-one-to-one-uni?userSSL=false";
 	String user = "hbstudent";
 	String pass = "hbstudent";
 	try {
