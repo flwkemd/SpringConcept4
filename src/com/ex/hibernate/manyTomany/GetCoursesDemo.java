e5c0ca6fff5e7a35c3b1663afc45843264d2a06e
flwkemd <flwkemd@naver.com>
2017-12-08 02:59:05 +0100

    @ManyToMany - Get

---
+++ src/com/ex/hibernate/manyTomany/GetCoursesDemo.java
@@ -0,0 +1,61 @@
+package com.ex.hibernate.manyTomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ex.hibernate.manyTomany.entity.Student;
+import com.ex.hibernate.manyTomany.entity.Course;
+import com.ex.hibernate.manyTomany.entity.Instructor;
+import com.ex.hibernate.manyTomany.entity.InstructorDetail;
+import com.ex.hibernate.manyTomany.entity.Review;
+
+
+public class GetCoursesDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// start a transaction
+			session.beginTransaction();
+
+			// get the student from database
+			int studentId = 2;
+			Student tempStudent = session.get(Student.class, studentId);
+			
+			System.out.println("\nLoaded student: "+tempStudent);
+			System.out.println("Course: "+tempStudent.getCourses());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
