c217def1abc41a4790a24fa9534750046ace249e
flwkemd <flwkemd@naver.com>
2017-12-08 01:08:00 +0100

    Hibernate Advanced Mappings - @OneToMany - Unidirectional

---
+++ src/com/ex/hibernate/oneTomany/CreateCoursesAndReviewsDemo.java
@@ -0,0 +1,67 @@
+package com.ex.hibernate.oneTomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ex.hibernate.oneTomany.entity.Course;
+import com.ex.hibernate.oneTomany.entity.Instructor;
+import com.ex.hibernate.oneTomany.entity.InstructorDetail;
+import com.ex.hibernate.oneTomany.entity.Review;
+
+
+public class CreateCoursesAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// start a transaction
+			session.beginTransaction();
+
+			// create a course
+			Course tempCourse = new Course("Pacman");
+			
+			// add some reviews
+			tempCourse.addReview(new Review("Great Course!"));
+			tempCourse.addReview(new Review("Cool"));
+			tempCourse.addReview(new Review("job well done"));
+			
+			// save the course ... and leverage the cascade all
+			System.out.println("Saving the course");
+			System.out.println(tempCourse);
+			System.out.println(tempCourse.getReviews());
+			
+			session.save(tempCourse);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
